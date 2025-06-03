701673e4a49ef0eedac9c89e15618024c77a153b
RodionCoach <25160654+RodionCoach@users.noreply.github.com>
2025-06-03 13:48:07 +0300

    feat: add vue ecosystem

---
+++ App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Vue</h1>
+  </div>
+</template>
+
+<script>
+
+import { mapState, mapMutations } from "vuex";
+
+export default {
+  computed: {
+    ...mapState([]),
+  },
+  methods: {
+    ...mapMutations([]),
+  },
+};
+</script>
